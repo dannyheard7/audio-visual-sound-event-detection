687fd960833ff3fee9e0ddbfa46391a699cd8405
Danny Heard <dannyheard7@gmail.com>
2018-02-06 18:31:24 +0000

    Add code to take frames from middle and end of videos

--- extract-video-frames-class-wise.py
+++ extract-video-frames-class-wise.py
@@ -42,25 +42,53 @@ def create_frame_filename(video_filename, frame_type):
 	return filename
 
 
-def take_frame_from_start(video_info, video_filename, output_folder):
+def take_frame_from_start(video_info, video_filename, video_location, output_folder):
 	frame_filename = os.path.join(output_folder, create_frame_filename(video_filename, "start"))
-	video_filename = os.path.join(config.video_training_data_location, video_filename) # Need to abstract so this can work for training, testing, evaluation...
+	video_filename = os.path.join(video_location, video_filename)
 
 	ffmpeg_string = "ffmpeg -i {}  -ss 00:00:00.1 -vframes 1 {}".format(video_filename, frame_filename)
 	os.system(ffmpeg_string)
 
 
-def take_frame_from_middle():
-	pass
+def take_frame_from_middle(video_info, video_filename, video_location, output_folder):
+	frame_filename = os.path.join(output_folder, create_frame_filename(video_filename, "middle"))
+	video_filename = os.path.join(video_location, video_filename)
+
+	middle_location = str((float(video_info[2]) - float(video_info[1])) / 2)
+	
+	# All videos are 10s or shorter
+	#if(not middle_location.startswith("10.")):
+	#	middle_location = "0" + middle_location
+
+	ffmpeg_string = "ffmpeg -i {}  -ss 00:00:0{} -vframes 1 {}".format(video_filename, middle_location, frame_filename)
+	os.system(ffmpeg_string)
 
 
-def take_frame_from_end():
-	pass
+def take_frame_from_end(video_info, video_filename, video_location, output_folder):
+	frame_filename = os.path.join(output_folder, create_frame_filename(video_filename, "end"))
+	video_filename = os.path.join(video_location, video_filename)
+
+	end_location = str((float(video_info[2]) - float(video_info[1])))
+
+	if(not end_location.startswith("10.")):
+		end_location = "0" + end_location
+	
+	ffmpeg_string = "ffmpeg -i {}  -ss 00:00:{} -vframes 1 {}".format(video_filename, end_location, frame_filename)
+	os.system(ffmpeg_string)
 
 
-def take_n_equal_spaced_frames(num_frames):
-	pass
+def take_n_equal_spaced_frames(num_frames, video_info video_filename, video_location, output_folder):
+	video_filename = os.path.join(video_location, video_filename)
 
+	filename = os.path.splitext(video_filename)[0] + "_spaced_%d" + config.video_frames_extension
+	frame_filename = os.path.join(output_folder, filename)
+	print(frame_filename)
+
+	video_length = float(video_info[2]) - float(video_info[1])
+
+	fps = 
+
+	ffmpeg_string = "ffmpeg -i {} -vf fps=1 {}".format(video_filename, frame_filename)
 
 def create_and_populate_csv():
 	pass
@@ -79,7 +107,7 @@ for class_label, class_name in class_labels.items():
 
 	for video_info in videos_by_classes[class_label]:
 		video_filename = get_video_filename(video_info, config.video_file_extension)
-		take_frame_from_start(video_info, video_filename, output_folder)
+		take_n_equal_spaced_frames(1, video_info, video_filename,  config.video_training_data_location, output_folder)
 
 
 #os.system('ffmpeg -i input.flv -ss 00:00:14.435 -vframes 1 out.png')
