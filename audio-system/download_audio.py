17c64f10fd558324062ecd88ee47dbe2752643d1
Danny Heard <dannyheard7@gmail.com>
2018-02-19 09:34:45 +0000

    Add download_audio code

---
+++ audio-system/download_audio.py
@@ -0,0 +1,121 @@
+#Code Contributor - Ankit Shah - ankit.tronix@gmail.com
+import pafy
+import time
+import datetime
+import itertools
+import os
+import sys
+import multiprocessing
+from multiprocessing import Pool
+from tqdm import tqdm
+
+#Format audio - 16 bit Signed PCM audio sampled at 44.1kHz
+def format_audio(input_audio_file,output_audio_file):
+	temp_audio_file = output_audio_file.split('.wav')[0] + '_temp.wav'
+	cmdstring = "ffmpeg -loglevel panic -i %s -ac 1 -ar 44100 %s" %(input_audio_file,temp_audio_file)
+	os.system(cmdstring)
+	cmdstring1 = "sox %s -G -b 16 -r 44100 %s" %(temp_audio_file,output_audio_file)
+	os.system(cmdstring1)
+
+	if os.path.exists(output_audio_file):
+		cmdstring2 = "rm -rf %s" %(temp_audio_file)
+		os.system(cmdstring2)
+	else:
+		print("Error converting audio file: " + temp_audio_file)
+
+#Trim audio based on start time and duration of audio. 
+def trim_audio(input_audio_file,output_audio_file,start_time,duration):
+	#print input_audio_file
+	#print output_audio_file
+	cmdstring = "sox %s %s trim %s %s" %(input_audio_file,output_audio_file,start_time,duration)
+	os.system(cmdstring)
+
+def multi_run_wrapper(args):
+   return download_audio_method(*args)
+
+#Method to download audio - Downloads the best audio available for audio id, calls the formatting audio function and then segments the audio formatted based on start and end time. 
+def download_audio_method(line,csv_file):
+	query_id = line.split(",")[0];
+	start_seconds = line.split(",")[1];
+	end_seconds = line.split(",")[2];
+	audio_duration = float(end_seconds) - float(start_seconds)
+
+	print("Query -> " + query_id)
+
+	url = "https://www.youtube.com/watch?v=" + query_id
+	try:
+		video = pafy.new(query_id)
+		bestaudio = video.getbestaudio()
+		#.csv - split - to get the folder information. As path is also passed for the audio - creating the directory from the path where this audio script is present. THus using second split to get the folder name where output files shall be downloaded
+		output_folder = sys.argv[1].split('.csv')[0].split("/")[-1] + "_" + csv_file.split('.csv')[0] +  "_" + "audio_downloaded"
+		#print output_folder
+		if not os.path.exists(output_folder):
+			os.makedirs(output_folder)
+		
+		path_to_download = output_folder + "/Y" + query_id + "." + bestaudio.extension
+		#print path_to_download
+		bestaudio.download(path_to_download)
+		
+		formatted_folder = sys.argv[1].split('.csv')[0].split("/")[-1] + "_" + csv_file.split('.csv')[0] + "_" + "audio_formatted_downloaded"
+		
+		if not os.path.exists(formatted_folder):
+			os.makedirs(formatted_folder)
+		
+		path_to_formatted_audio = formatted_folder + "/Y" + query_id + ".wav"
+		
+		format_audio(path_to_download,path_to_formatted_audio)
+		
+		#Trimming code
+		segmented_folder = sys.argv[1].split('.csv')[0].split("/")[-1] + "_" + csv_file.split('.csv')[0] +  "_" + "audio_formatted_and_segmented_downloads"
+		if not os.path.exists(segmented_folder):
+			os.makedirs(segmented_folder)
+		
+		path_to_segmented_audio = segmented_folder + "/Y" + query_id + '_' + start_seconds + '_' + end_seconds +  ".wav"
+		trim_audio(path_to_formatted_audio,path_to_segmented_audio,start_seconds,audio_duration)
+
+		if os.path.exists(path_to_segmented_audio):
+			#Remove the original audio 
+			delete_original_file_cmd = "rm -f {0}".format(path_to_download)
+			os.system(delete_original_file_cmd)
+
+			# Remove the formatted audio
+			delete_original_file_cmd = "rm -f {0}".format(path_to_formatted_audio)
+			os.system(delete_original_file_cmd)
+		else:
+			print("Error converting audio file: " + path_to_download)
+
+		ex1 = ""
+	except Exception as ex:
+		ex1 = str(ex) + ',' + str(query_id)
+		print("Error is ---> " + str(ex))
+	return ex1
+
+
+def download_audio(csv_file,timestamp):	
+	segments_info = []
+	
+	with open(csv_file, "r") as segments_info_file:	
+		for line in segments_info_file:
+			segments_info.append((line, csv_file))
+
+	cpu_count = multiprocessing.cpu_count()	
+
+	P = multiprocessing.Pool(processes=cpu_count)
+	exception = P.map(multi_run_wrapper, segments_info)
+
+	for item in exception:
+		if item:
+			print((str(item)))
+
+	P.close()
+	P.join()
+
+if __name__ == "__main__":
+	if len(sys.argv) !=2:
+		print('takes arg1 as csv file to downloaded')
+	else:
+		
+		ts = time.time()
+		timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M_%S')			
+		download_audio(sys.argv[1],timestamp)
+	
--- video/download_video.py
+++ video/download_video.py
@@ -1,4 +1,3 @@
-#Code Contributor - Ankit Shah - ankit.tronix@gmail.com
 import pafy
 import time
 import datetime
@@ -11,7 +10,7 @@ from tqdm import tqdm
 
 #Format video - Convert to H.264 and trim
 def format_video(input_video_file, output_video_file, start_time, duration):
-	cmdstring = "ffmpeg -loglevel panic -i {0} -ss 00:00:{1} -t 00:00:{2} {3}".format(input_video_file, start_time, duration, output_video_file)
+	cmdstring = "ffmpeg -loglevel panic -i {0} -ss {1} -t {2} {3}".format(input_video_file, start_time.split(".")[0], duration, output_video_file)
 	print(cmdstring)
 	os.system(cmdstring)
 
@@ -23,11 +22,9 @@ def download_video_method(line,csv_file):
 	query_id = line.split(",")[0];
 	start_seconds = line.split(",")[1];
 	end_seconds = line.split(",")[2];
-	video_duration = float(end_seconds) - float(start_seconds)
+	video_duration = float(end_seconds.split(".")[0]) - float(start_seconds.split(".")[0])
 	#positive_labels = ','.join(line.split(",")[3:]);
 	print("Query -> " + query_id)
-	
-	url = "https://www.youtube.com/watch?v=" + query_id
 
 	segmented_folder = sys.argv[1].split('.csv')[0].split("/")[-1] + "_" + csv_file.split('.csv')[0] +  "_" + "video_formatted_and_segmented_downloads"
 		
@@ -38,8 +35,8 @@ def download_video_method(line,csv_file):
 
 	if not os.path.isfile(path_to_segmented_video):
 		try:
-			video = pafy.new(url)
-			bestvideo = video.getbestvideo()
+			video = pafy.new(query_id)
+			bestvideo = video.getbestvideo() # Maybe some videos don't have streams without audio? so can try downloading with audio
 
 			#.csv - split - to get the folder information. As path is also passed for the video - creating the directory from the path where this video script is present. THus using second split to get the folder name where output files shall be downloaded
 			output_folder = sys.argv[1].split('.csv')[0].split("/")[-1] + "_" + csv_file.split('.csv')[0] +  "_" + "video_downloaded"
@@ -49,24 +46,25 @@ def download_video_method(line,csv_file):
 				os.makedirs(output_folder)
 			
 			path_to_download = output_folder + "/Y" + query_id + "." + bestvideo.extension
-			bestvideo.download(path_to_download)			
+			bestvideo.download(path_to_download, quiet=True)			
 				
 			format_video(path_to_download, path_to_segmented_video, start_seconds, video_duration)
 
-			#Remove the original video 
-			delete_original_file_cmd = "rm -f {0}".format(path_to_download)
-			os.system(delete_original_file_cmd)
+			if os.path.isfile(path_to_segmented_video):
+				#Remove the original video 
+				delete_original_file_cmd = "rm -f {0}".format(path_to_download)
+				os.system(delete_original_file_cmd)
+			else:
+				print("Error converting file: " + path_to_download)
 
 			ex1 = ""
 		except Exception as ex:
 			ex1 = str(ex) + ',' + str(query_id)
-			print ("Error is ---> " + str(ex))
+			print ("Error is ---> " + str(ex))  
 		return ex1
 
 
 def download_video(csv_file, timestamp):	
-	print(multiprocessing.cpu_count())
-
 	segments_info = []
 	
 	with open(csv_file, "r") as segments_info_file:	
