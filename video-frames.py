31176a37ca349889f84d4feb9b41e61431d75e32
Danny Heard <dannyheard7@gmail.com>
2018-02-26 15:36:25 +0000

    Save predictions for frames for graph

--- video-frames.py
+++ video-frames.py
@@ -5,6 +5,7 @@ import numpy as np
 import pickle
 from keras.preprocessing import image
 from keras.applications.inception_v3 import InceptionV3, preprocess_input, decode_predictions
+from collections import defaultdict
 
 import config
 import meta
@@ -67,20 +68,15 @@ def take_frame_each_scene(video_info, video_filename, video_location, output_fol
     pass
 
 
-def extract_image_features(model, image_path, features_output_path):
+def extract_image_features(model, image_path):
     img = image.load_img(image_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
 
     preds = model.predict(x)
-    print(np.shape(preds))
-    # decode the results into a list of tuples (class, description, probability)
-    # print('Predicted:', decode_predictions(preds, top=3)[0])
-
-    # Do i need to do PCA?
-    pickle.dump(preds, open(features_output_path, 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
-
+    return preds
+   
 
 def get_video_frames(videos_location, output_folder, data_csv_file):
     FileIO.create_folder(output_folder)
@@ -88,7 +84,8 @@ def get_video_frames(videos_location, output_folder, data_csv_file):
     features_output_path = os.path.join(output_folder, "features/")
     FileIO.create_folder(features_output_path)
 
-    videos_info = FileIO.load_csv_as_list(data_csv_file)
+    class_labels = meta.load_sound_event_classes()
+    videos_by_classes = meta.load_videos_info(videos_location, data_csv_file)
     model = InceptionV3(weights='imagenet')
 
     for video_info in videos_info:
@@ -100,17 +97,47 @@ def get_video_frames(videos_location, output_folder, data_csv_file):
             frame_filename = take_frame_from_start(video_info, video_path, output_folder)
 
             if os.path.exists(frame_filename):
-                extract_image_features(model, frame_filename, frame_features_path)
+                preds = extract_image_features(model, frame_filename)
+                pickle.dump(preds, open(features_output_path, 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
                 os.remove(frame_filename)
+
+
+def plot_preds_by_class(videos_location, output_folder, data_csv_file):
+    FileIO.create_folder(output_folder)
+    
+    class_labels = meta.load_sound_event_classes()
+    videos_by_classes = meta.load_videos_info_by_class(data_csv_file)
+    model = InceptionV3(weights='imagenet')
+    class_predictions =  defaultdict(list)
+
+    for class_label, class_name in class_labels.items():
+        for video_info in videos_by_classes[class_label]:
+	        video_filename = FileIO.get_video_filename(video_info[0], video_info[1], video_info[2], config.video_file_extension)
+	        video_path = os.path.join(videos_location, video_filename)
+
+	        if os.path.exists(video_path):
+	            frame_filename = take_frame_from_start(video_info, video_path, output_folder)
+
+	            if os.path.exists(frame_filename):
+	                preds = extract_image_features(model, frame_filename)
+	                os.remove(frame_filename)
+
+	                decoded = decode_predictions(preds, top=3)[0]
+	                for prediction in decoded:
+	                    class_predictions[class_name].append((prediction[1], prediction[2]))
+
+    print(class_predictions)
+    pickle.dump(class_predictions, open("predictions.pkl", 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest='mode')
 
-    parser_train = subparsers.add_parser('train')
-    parser_recognize = subparsers.add_parser('test')
-    parser_recognize = subparsers.add_parser('evaluate')
+    subparsers.add_parser('train-preds-plot')
+    subparsers.add_parser('train')
+    subparsers.add_parser('test')
+    subparsers.add_parser('evaluate')
 
     args = parser.parse_args()
 
@@ -120,3 +147,5 @@ if __name__ == "__main__":
         get_video_frames(config.video_testing_data_location, config.video_testing_frames_location, config.testing_data_csv_file)
     elif args.mode == 'evaluate':
         get_video_frames(config.video_evaluation_data_location, config.video_evaluation_frames_location, config.evaluation_data_csv_file)
+    elif args.mode == 'train-preds-plot':
+    	plot_preds_by_class(config.video_training_data_location, config.video_training_frames_location, config.training_data_csv_file)
