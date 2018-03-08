2a6c10a67f1f6671aeaaca3c3ed26b15562cf26f
Danny Heard <dannyheard7@gmail.com>
2018-03-08 14:05:19 +0000

    Refactor strucutre

---
+++ plot-imagenet-preds.py
@@ -0,0 +1,98 @@
+import argparse
+import collections
+import dill as pickle
+import matplotlib as mpl
+import os
+from keras.applications import InceptionV3
+from keras.applications.imagenet_utils import decode_predictions
+
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import math
+import video_frames
+
+import FileIO
+import config
+import meta
+
+
+def plot_preds_by_class(videos_location, output_folder, data_csv_file):
+    FileIO.create_folder(output_folder)
+
+    class_labels = meta.load_sound_event_classes()
+    videos_by_classes = meta.load_videos_info_by_class(data_csv_file)
+    model = InceptionV3(weights='imagenet')
+    class_predictions = collections.defaultdict(lambda: collections.defaultdict(int))
+    num_frames = 3
+
+    for class_label, class_name in class_labels.items():
+        for video_info in videos_by_classes[class_label]:
+            video_filename = FileIO.get_video_filename(video_info[0], video_info[1], video_info[2],
+                                                       config.video_file_extension)
+            video_path = os.path.join(videos_location, video_filename)
+
+            if os.path.exists(video_path):
+                frame_filenames = video_frames.take_n_equal_spaced_frames(num_frames, video_info, video_path, output_folder)
+
+                for frame_filename in frame_filenames:
+                    print(frame_filename)
+                    if os.path.exists(frame_filename):
+                        preds = video_frames.extract_image_features(model, frame_filename)
+                        os.remove(frame_filename)
+
+                        decoded = decode_predictions(preds, top=3)[0]
+                        for prediction in decoded:
+                            class_predictions[class_name][prediction[1]] += 1  # , prediction[2]))
+
+    for class_name, prediction_names in class_predictions.items():
+        for prediction_name in prediction_names:
+            class_predictions[class_name][prediction_name] /= num_frames
+
+    print(class_predictions)
+    pickle.dump(class_predictions, open("predictions.pkl", 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def plot_on_graph(predictions_file, output_file):
+    predictions = pickle.load(open(predictions_file, "rb" ))
+
+    num_classes = len(predictions)
+    cols = 2
+    rows = math.ceil(num_classes / cols)
+    fig, axarr = plt.subplots(rows, cols, figsize=(185, 40))
+
+    # Remove values that are less than one so graphs are not as wide
+
+    dcase_labels = list(predictions.keys())
+    count = 0
+    for i in range(0, rows):
+        for j in range(0, cols):
+            if len(dcase_labels) == count:
+                break
+
+            class_name = dcase_labels[count]
+            class_predictions = predictions[class_name]
+            dict_len = len(class_predictions)
+
+            axarr[i, j].bar(range(dict_len), list(class_predictions.values()), align='center')
+            axarr[i, j].set_title("ImageNet Predicitions for {}".format(class_name))
+            axarr[i, j].set_xticks(range(dict_len))
+            axarr[i, j].set_xticklabels(list(class_predictions.keys()), rotation='vertical')
+            count += 1
+
+    fig.tight_layout()
+    plt.savefig(output_file)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='mode')
+
+    subparsers.add_parser('predict')
+    subparsers.add_parser('plot-predictions')
+    args = parser.parse_args()
+
+    if args.mode == 'predict':
+        plot_preds_by_class(config.video_training_data_location, config.video_training_frames_location,
+                            config.training_data_csv_file)
+    elif args.mode == 'plot-predictions':
+        plot_on_graph("predictions.pkl", "predictions-bar-chart.png")
--- video_frames.py
+++ video_frames.py
@@ -1,15 +1,13 @@
-import os
-import numpy as np
 import argparse
-import numpy as np 
-import dill as pickle
-from keras.preprocessing import image
-from keras.applications.inception_v3 import InceptionV3, preprocess_input, decode_predictions
-from collections import defaultdict
+import os
 
-import config
-import meta
+import dill as pickle
+import numpy as np
+from keras.applications.inception_v3 import InceptionV3, preprocess_input
+from keras.preprocessing import image
+
 import FileIO
+import config
 
 
 def take_frame_from_start(video_path, output_folder):
@@ -37,7 +35,7 @@ def take_frame_from_end(video_info, video_path, output_folder):
 
     end_location = str((float(video_info[2]) - float(video_info[1])))
 
-    if(not end_location.startswith("10.")):
+    if not end_location.startswith("10."):
         end_location = "0" + end_location
         
     ffmpeg_string = "ffmpeg -i {}  -ss 00:00:{} -vframes 1 {}".format(video_path, end_location, frame_filename)
@@ -58,11 +56,10 @@ def take_n_equal_spaced_frames(num_frames, video_info, video_path, output_folder
 
     filenames = []
 
-    for i in range(0, num_frames - 1):
-        frame_filename.append(frame_filename % i)
+    for i in range(1, num_frames + 2):
+        filenames.append(frame_filename % i)
 
     return filenames
-
 
 def take_frame_each_scene(video_info, video_filename, video_location, output_folder):
     pass
@@ -101,50 +98,16 @@ def get_video_frames(videos_location, output_folder, data_csv_file):
                 os.remove(frame_filename)
 
 
-def plot_preds_by_class(videos_location, output_folder, data_csv_file):
-    FileIO.create_folder(output_folder)
-    
-    class_labels = meta.load_sound_event_classes()
-    videos_by_classes = meta.load_videos_info_by_class(data_csv_file)
-    model = InceptionV3(weights='imagenet')
-    class_predictions = defaultdict(lambda: defaultdict(int))
-
-    for class_label, class_name in class_labels.items():
-        for video_info in videos_by_classes[class_label]:
-            video_filename = FileIO.get_video_filename(video_info[0], video_info[1], video_info[2], config.video_file_extension)
-            video_path = os.path.join(videos_location, video_filename)
-
-            if os.path.exists(video_path):
-                frame_filename = take_frame_from_start(video_path, output_folder)
-
-                if os.path.exists(frame_filename):
-                    preds = extract_image_features(model, frame_filename)
-                    os.remove(frame_filename)
-
-                    decoded = decode_predictions(preds, top=3)[0]
-                    for prediction in decoded:
-                        class_predictions[class_name][prediction[1]]  += 1 #, prediction[2]))
-
-    print(class_predictions)
-    pickle.dump(class_predictions, open("predictions.pkl", 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest='mode')
 
-    subparsers.add_parser('train-preds-plot')
-    subparsers.add_parser('train')
-    subparsers.add_parser('test')
-    subparsers.add_parser('evaluate')
+    parser_get_frames = subparsers.add_parser("get_frames")
+    parser_get_frames.add_argument('--videos_location', type=str)
+    parser_get_frames.add_argument('--frames_location', type=str)
+    parser_get_frames.add_argument('--csv_file', type=str)
 
     args = parser.parse_args()
 
-    if args.mode == 'train':
-        get_video_frames(config.video_training_data_location, config.video_training_frames_location, config.training_data_csv_file)
-    elif args.mode == 'test':
-        get_video_frames(config.video_testing_data_location, config.video_testing_frames_location, config.testing_data_csv_file)
-    elif args.mode == 'evaluate':
-        get_video_frames(config.video_evaluation_data_location, config.video_evaluation_frames_location, config.evaluation_data_csv_file)
-    elif args.mode == 'train-preds-plot':
-    	plot_preds_by_class(config.video_training_data_location, config.video_training_frames_location, config.training_data_csv_file)
+    if args.mode == 'get_frames':
+        get_video_frames(args.videos_location, args.frames_location, args.csv_file)
